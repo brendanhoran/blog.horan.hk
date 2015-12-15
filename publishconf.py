bd2e7ffbaf0584dfeb37ef402c393f88decebf6c
Brendan Horan <brendan@horan.hk>
2015-12-15 22:48:05 +0800

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -18,6 +18,7 @@ FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 
 # append the rss link to the social sidebar
+# you must use the VARIABLE syntax for the atom feed, vs the actual URL
 SOCIAL = SOCIAL + (('RSS feed', SITEURL + '/' + FEED_ALL_ATOM , 'rss'),)
 
 # disable for prod
